0a0ca3c8a86343f3facf56e630f85b9b13a4b70a
num73 <xlnum73@outlook.com>
2025-07-08 19:35:21 +0800

    add gtest

---
+++ gtest/test_util.c
@@ -0,0 +1,29 @@
+#include <glib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <limits.h>
+
+#include "util.h"
+
+static void test_util_get_fullpath(void) {
+
+    char *fullpath = malloc(PATH_MAX);
+    util_get_fullpath("test.txt", fullpath);
+    g_assert_nonnull(fullpath);
+    g_assert_cmpstr(fullpath, ==, "/home/num73/nufs/build/gtest/test.txt");
+    g_free(fullpath);
+}
+
+int main(int argc, char *argv[]) {
+
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_set_nonfatal_assertions();
+
+    g_test_add_func("/util/get_fullpath", test_util_get_fullpath);
+    
+    return g_test_run();
+}
